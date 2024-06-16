8019e204a080db27987856896c14c573fe07adf7
evereux <evereux@gmail.com>
2024-06-16 13:45:11 +0100

    add active document details to page header also add html filenames html header

--- application/views/htmx/document_queries.py
+++ application/views/htmx/document_queries.py
@@ -1,11 +1,46 @@
 from pathlib import Path
 
 from flask import render_template
+from pycatia.in_interfaces.document import Document
+from pycatia.mec_mod_interfaces.part_document import PartDocument
+from pycatia.drafting_interfaces.drawing_document import DrawingDocument
+from pycatia.product_structure_interfaces.product_document import ProductDocument
 from pycatia.exception_handling.exceptions import CATIAApplicationException
 
 from application import app
 from application.pycatia_scripts.the_document import PTActiveDocument
 from application.views.url_prefixes import htmx
+
+@app.route(f'{htmx}/document/object', methods=['GET'])
+def htmx_document_object():
+    document_type: str = None
+    document_object: Document = None
+    try:
+        pt_active_document = PTActiveDocument()
+        document: Document = pt_active_document.active_document
+        cls = type(document)
+        module = cls.__module__
+        name = cls.__qualname__
+        if name == ProductDocument.__name__:
+            document_type = "document"
+            product_document = ProductDocument(document.com_object)
+            document_object = product_document.product
+        elif name == PartDocument.__name__:
+            document_type = "document"
+            part_document = PartDocument(document.com_object)
+            document_object = part_document.product
+        elif name == DrawingDocument.__name__:
+            document_type = "drawing"
+            document_object = DrawingDocument(document.com_object)
+        else:
+            document_type = "document"
+            document_object = document
+        return render_template('partials/document_object.html', document_type=document_type, document_object=document_object)
+    except CATIAApplicationException:
+        return render_template('partials/document_object.html', document_type=document_type)
+    
+    return render_template('partials/document_object.html', document_type=document_type)
+
 
 @app.route(f'{htmx}/document/path', methods=['GET'])
 def htmx_document_path():
